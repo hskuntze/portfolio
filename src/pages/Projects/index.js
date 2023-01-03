79388a0096581b8d8415e30de580814b0827e83d
hskuntze <hskuntze@gmail.com>
2023-01-03 13:14:20 -0300

    projects page finalizada

--- src/pages/Projects/index.js
+++ src/pages/Projects/index.js
@@ -29,6 +29,11 @@ import hkc2 from '../../assets/images/hkc2.png';
 import hkc3 from '../../assets/images/hkc3.png';
 import hkc4 from '../../assets/images/hkc4.png';
 
+//Musik Images
+import mk1 from '../../assets/images/mk_1.png';
+import mk2 from '../../assets/images/mk_2.png';
+import mk3 from '../../assets/images/mk_3.png';
+
 const Projects = () => {
   return (
     <main id="projects-main">
@@ -37,31 +42,39 @@ const Projects = () => {
         <section className="section">
           <div className="project-info">
             <div className="left-side-info">
-              <div className="up-left">
-                <img src={BulletJournalLogo} alt="logotipo bullet journal" />
-                <a
-                  href="https://bullet-journal-frontend.herokuapp.com/"
-                  target={'_blank'}
-                  rel="noreferrer"
-                >
-                  Acesse
-                </a>
+              <div className="status-alert bg-success">
+                Finalizado -<span className="fst-italic">&nbsp;Finished</span>
+              </div>
+              <div className="up">
+                <div className="up-left">
+                  <a
+                    href="https://bullet-journal-frontend.herokuapp.com/"
+                    target={'_blank'}
+                    rel="noreferrer"
+                  >
+                    <img
+                      src={BulletJournalLogo}
+                      alt="logotipo bullet journal"
+                    />
+                  </a>
+                </div>
+                <div className="up-right">
+                  <span>Front-end: React.js + Typescript</span>
+                  <span>Back-end: Java + Spring Boot</span>
+                  <span>Versionamento: GIT @ GitHub</span>
+                  <span>Banco de Dados: PostgreSQL</span>
+                  <span>Hospedagem: Heroku</span>
+                  <span>Hospedagem de Banco de Dados: Heroku</span>
+                </div>
               </div>
               <div className="down-left">
                 O Bullet Journal é uma iniciativa pessoal que nasce com a
                 perspectiva da importância de um controle da rotina, bem como um
-                espaço pessoal. As funcionalidades abordam as principais
-                utilidades de um caderno diário: objetivos a serem cumpridos, os
-                "to-dos", um espaço para anotações, os "cards", bem como manter
-                anotado a sequência diária de atividades cumpridas, os
-                "streaks".
+                espaço pessoal.
                 <div className="hr"></div>
                 The Bullet Journal is a personal initiative which is born with
                 the notion of importance to maintain a routine, as well as a
-                personal space. It's functionalities covers the very much
-                essensce of a journal: daily maing goals to achieve, "to-dos", a
-                space to make annotations, the "cards", and the recording the
-                evolution of setted up tasks, "streaks".
+                personal space.
               </div>
             </div>
             <div className="right-side-info">
@@ -161,11 +174,25 @@ const Projects = () => {
         <section className="section">
           <div className="project-info">
             <div className="left-side-info">
-              <div className="up-left">
-                <img src={MusikLogo} alt="logotipo musik" />
-                <a href="www.google.com" target={'_blank'} rel="noreferrer">
-                  Acesse
-                </a>
+              <div className="status-alert bg-warning text-bg-warning">
+                Em desenvolvimento -
+                <span className="fst-italic">&nbsp;In development</span>
+              </div>
+              <div className="up">
+                <div className="up-left">
+                  <a
+                    href="https://kuntzeprojects-musik.netlify.app/"
+                    target={'_blank'}
+                    rel="noreferrer"
+                  >
+                    <img src={MusikLogo} alt="logotipo musik" />
+                  </a>
+                </div>
+                <div className="up-right">
+                  <span>Front-end: React.js</span>
+                  <span>Versionamento: GIT @ GitHub</span>
+                  <span>Hospedagem: Netlify</span>
+                </div>
               </div>
               <div className="down-left">
                 O Musik é um aplicativo de transferência de músicas entre as
@@ -176,7 +203,83 @@ const Projects = () => {
                 vice versa.
               </div>
             </div>
-            <div className="right-side-info">{/* Carousel */}</div>
+            <div className="right-side-info">
+              <div
+                id="musikCarousel"
+                className="carousel slide"
+                data-bs-ride="true"
+              >
+                <div className="carousel-indicators">
+                  <button
+                    type="button"
+                    data-bs-target="#musikCarousel"
+                    data-bs-slide-to="0"
+                    className="active"
+                    aria-current="true"
+                    aria-label="Slide 1"
+                  ></button>
+                  <button
+                    type="button"
+                    data-bs-target="#musikCarousel"
+                    data-bs-slide-to="1"
+                    aria-label="Slide 2"
+                  ></button>
+                  <button
+                    type="button"
+                    data-bs-target="#musikCarousel"
+                    data-bs-slide-to="2"
+                    aria-label="Slide 3"
+                  ></button>
+                </div>
+                <div className="carousel-inner">
+                  <div className="carousel-item active">
+                    <img
+                      src={mk1}
+                      className="d-block w-100"
+                      alt="imagem home musik"
+                    />
+                  </div>
+                  <div className="carousel-item">
+                    <img
+                      src={mk2}
+                      className="d-block w-100"
+                      alt="imagem dashboard musik"
+                    />
+                  </div>
+                  <div className="carousel-item">
+                    <img
+                      src={mk3}
+                      className="d-block w-100"
+                      alt="imagem seção spotify musik"
+                    />
+                  </div>
+                </div>
+                <button
+                  className="carousel-control-prev"
+                  type="button"
+                  data-bs-target="#musikCarousel"
+                  data-bs-slide="prev"
+                >
+                  <span
+                    className="carousel-control-prev-icon"
+                    aria-hidden="true"
+                  ></span>
+                  <span className="visually-hidden">Previous</span>
+                </button>
+                <button
+                  className="carousel-control-next"
+                  type="button"
+                  data-bs-target="#musikCarousel"
+                  data-bs-slide="next"
+                >
+                  <span
+                    className="carousel-control-next-icon"
+                    aria-hidden="true"
+                  ></span>
+                  <span className="visually-hidden">Next</span>
+                </button>
+              </div>
+            </div>
           </div>
         </section>
 
@@ -184,15 +287,27 @@ const Projects = () => {
         <section className="section">
           <div className="project-info">
             <div className="left-side-info">
-              <div className="up-left">
-                <img src={HKLearnLogo} alt="logotipo hk learn" />
-                <a
-                  href="https://hklearn.herokuapp.com/"
-                  target={'_blank'}
-                  rel="noreferrer"
-                >
-                  Acesse
-                </a>
+              <div className="status-alert bg-success">
+                Finalizado -<span className="fst-italic">&nbsp;Finished</span>
+              </div>
+              <div className="up">
+                <div className="up-left">
+                  <a
+                    href="https://hklearn.herokuapp.com/"
+                    target={'_blank'}
+                    rel="noreferrer"
+                  >
+                    <img src={HKLearnLogo} alt="logotipo hk learn" />
+                  </a>
+                </div>
+                <div className="up-right">
+                  <span>Front-end: React.js + Typescript</span>
+                  <span>Back-end: Java + Spring Boot</span>
+                  <span>Versionamento: GIT @ GitHub</span>
+                  <span>Banco de Dados: PostgreSQL</span>
+                  <span>Hospedagem: Heroku</span>
+                  <span>Hospedagem de Banco de Dados: AWS <span className="text-muted fst-italic">Desabilitado</span></span>
+                </div>
               </div>
               <div className="down-left">
                 O HKLearn foi criado com o intuito de oferecer uma plataforma de
@@ -326,15 +441,28 @@ const Projects = () => {
         <section className="section">
           <div className="project-info">
             <div className="left-side-info">
-              <div className="up-left">
-                <img src={KuntzeVendasLogo} alt="logotipo kuntze vendas" />
-                <a
-                  href="https://kuntzevendas.netlify.app/"
-                  target={'_blank'}
-                  rel="noreferrer"
-                >
-                  Acesse
-                </a>
+              <div className="status-alert bg-success">
+                Finalizado -<span className="fst-italic">&nbsp;Finished</span>
+              </div>
+              <div className="up">
+                <div className="up-left">
+                  <a
+                    href="https://kuntzevendas.netlify.app/"
+                    target={'_blank'}
+                    rel="noreferrer"
+                  >
+                    <img src={KuntzeVendasLogo} alt="logotipo kuntze vendas" />
+                  </a>
+                </div>
+                <div className="up-right">
+                  <span>Front-end: React.js + Typescript</span>
+                  <span>Back-end: Java + Spring Boot</span>
+                  <span>Versionamento: GIT @ GitHub</span>
+                  <span>Banco de Dados: PostgreSQL</span>
+                  <span>Hospedagem Front-end: Netlify</span>
+                  <span>Hospedagem Back-end: Heroku</span>
+                  <span>Hospedagem de Banco de Dados: AWS <span className="text-muted fst-italic">Desabilitado</span></span>
+                </div>
               </div>
               <div className="down-left">
                 KuntzeVendas é um projeto de demonstração que exibe uma tela de
@@ -416,15 +544,28 @@ const Projects = () => {
         <section className="section">
           <div className="project-info">
             <div className="left-side-info">
-              <div className="up-left">
-                <img src={HKCatalogLogo} alt="logotipo hk catalog" />
-                <a
-                  href="https://hkcatalog.netlify.app/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  Acesse
-                </a>
+              <div className="status-alert bg-success">
+                Finalizado -<span className="fst-italic">&nbsp;Finished</span>
+              </div>
+              <div className="up">
+                <div className="up-left">
+                  <a
+                    href="https://hkcatalog.netlify.app/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <img src={HKCatalogLogo} alt="logotipo hk catalog" />
+                  </a>
+                </div>
+                <div className="up-right">
+                  <span>Front-end: React.js + Typescript</span>
+                  <span>Back-end: Java + Spring Boot</span>
+                  <span>Versionamento: GIT @ GitHub</span>
+                  <span>Banco de Dados: PostgreSQL</span>
+                  <span>Hospedagem Front-end: Netlify</span>
+                  <span>Hospedagem Back-end: Heroku</span>
+                  <span>Hospedagem de Banco de Dados: AWS <span className="text-muted fst-italic">Desabilitado</span></span>
+                </div>
               </div>
               <div className="down-left">
                 HKCatalog é um aplicativo que demonstra um catálogo de produtos
